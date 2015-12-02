98c60e405f9653ff3f03b5d17b10e81be65223c0
Lee <lkatsumata1@toromail.csudh.edu>
2015-12-02 00:04:06 -0800

    Can edit user profile data (username, password, etc.) to DB. Can view edit address, but that's it for now.

--- src/com/nds/pkg/EmpEditSrvlt.java
+++ src/com/nds/pkg/EmpEditSrvlt.java
@@ -26,8 +26,7 @@ public class EmpEditSrvlt extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
 	}
 
 	/**
@@ -39,8 +38,9 @@ public class EmpEditSrvlt extends HttpServlet {
 		if(e.equals("Edit User Info"))
 			request.getRequestDispatcher("/EmployeeEdit.jsp").forward(request, response);
 		else{
-			
+			if(e.equals("Edit Address")){
+				request.getRequestDispatcher("/EmployeeEditAddress.jsp").forward(request, response);
+			}
 		}
-		request.getRequestDispatcher("/EmployeeEdit.jsp").forward(request, response);
 	}
 }
